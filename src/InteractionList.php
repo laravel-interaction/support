468b7cc67d36b535d245b77d72ce1d05d15c4a67
Zing <zingimmick@outlook.com>
2021-03-17 17:20:13 +0800

    Refactor ServiceProvider (#6)

---
+++ src/InteractionList.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LaravelInteraction\Support;
+
+class InteractionList
+{
+    public const BOOKMARK = 'bookmark';
+
+    public const SUBSCRIBE = 'subscribe';
+
+    public const LIKE = 'like';
+
+    public const VOTE = 'vote';
+}
